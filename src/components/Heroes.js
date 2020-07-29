0eb5f8e0adc8517e874f727b7a46c2af02e5fed8
Jonathan Won <jabinx92@gmail.com>
2020-07-29 14:53:24 -0700

    implement map heroes

---
+++ src/components/Heroes.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Heroes = (props) => (
+    <div>
+        <img src={"http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + props.id + "_0.jpg"} alt=""/>
+    </div>
+)
+
+export default Heroes
--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,34 +1,48 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
+import Heroes from './Heroes'
+
+// const URL = "http://ddragon.leagueoflegends.com/cdn/10.15.1/data/en_US/champion.json";
 
 class MainPage extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            info: []
+            data: []
         }
     }
 
-
     componentDidMount() {
         const proxyurl = "https://cors-anywhere.herokuapp.com/";
-        const url = "https://na1.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=RGAPI-d7e57a1c-a9de-4cac-95ae-c473e1df6bd1"; // site that doesn’t send Access-Control-*
+        const url = "http://ddragon.leagueoflegends.com/cdn/10.15.1/data/en_US/champion.json"; // site that doesn’t send Access-Control-*
             fetch(proxyurl + url) // https://cors-anywhere.herokuapp.com/https://example.com
                 .then(response => response.text())
                 .then(contents => {
-                    this.state.info.push(contents)
-                    console.log(contents)})
-                .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
-                
+                    this.state.data.push(contents)
+                    console.log(contents)
+                })
+                .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))   
     }
 
+    // componentDidMount() {
+    //     axios.get(URL)
+    //         .then(res => {
+    //             this.setState({
+    //                 data: res
+    //             });
+    //         });
+    //     console.log(this.state.data)
+    // }
+
+    
+
     render() {
+        const Loader = this.state.data.map(loader => (
+            <Heroes id={loader.data}/>
+        ))
         return (
             <div>
-                The input you submitted is: {this.props.username}
-                <ul>
-                {this.state.info.map(thing => <li>{thing.username}</li>)}
-                </ul>
+                {Loader}
             </div>
         )
     }
