7d4ca9bd203cde862a624a10fddfb56cfe5ca3e2
Jonathan Won <jabinx92@gmail.com>
2020-10-01 20:41:26 -0700

    tidy up, make project with client and server files

--- server/server.js
+++ server/server.js
@@ -33,6 +33,6 @@ app.get(`/api/summonerRank/:id`, (req, res) => {
 });
 
 
-const port = 8080;
+const port = 5000;
 
-app.listen(port, () => console.log(`Server started on port ${port}`))
+app.listen(port, '0.0.0.0')
