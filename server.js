819846552493041be38eacd3187257717ab17cb9
Jonathan Won <jabinx92@gmail.com>
2020-10-05 14:21:39 -0700

    new key

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get(`/api/:username`, (req, res) => {
 
     console.log('req.params + ' +req.params.username)
 
-    fetch('https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/' +req.params.username+ '?api_key=RGAPI-031ffc06-01a2-4696-9e40-235d76f08cc1')
+    fetch('https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/' +req.params.username+ '?api_key=RGAPI-50d7d72e-ed68-428b-a6d3-44545c6c3a44')
     .then(res => res.json())
     .then(result => res.json(result));
 });
@@ -20,7 +20,7 @@ app.get(`/api/getHero/:accountId`, (req, res) => {
 
     console.log(`req.params + ` + req.params.accountId)
 
-    fetch('https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/' +req.params.accountId+ '?endIndex=20&api_key=RGAPI-031ffc06-01a2-4696-9e40-235d76f08cc1')
+    fetch('https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/' +req.params.accountId+ '?endIndex=20&api_key=RGAPI-50d7d72e-ed68-428b-a6d3-44545c6c3a44')
     .then(res => res.json())
     .then(result => res.json(result));
 });
@@ -30,7 +30,7 @@ app.get(`/api/summonerRank/:id`, (req, res) => {
 
     console.log(`req.params + ` + req.params.id)
 
-    fetch('https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/'+req.params.id+'?api_key=RGAPI-031ffc06-01a2-4696-9e40-235d76f08cc1')
+    fetch('https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/'+req.params.id+'?api_key=RGAPI-50d7d72e-ed68-428b-a6d3-44545c6c3a44')
     .then(res => res.json())
     .then(result => res.json(result));
 });
